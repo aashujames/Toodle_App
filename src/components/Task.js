72298a9d6dc9ca306383ec73812f63980362f84a
Ashish Gupta <ashishgupt25@gmail.com>
2022-06-09 17:41:21 +0530

    Deleting a task

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import ClearIcon from "@mui/icons-material/Clear";
 
 const Task = ({ task, id, setTaskList, taskList }) => {
     const [input, setInput] = useState(task);
@@ -10,15 +11,25 @@ const Task = ({ task, id, setTaskList, taskList }) => {
         setTaskList(temp);
     };
 
+    const removeTask = () => {
+        const filtered = taskList.filter((item) => item.id !== id);
+        setTaskList(filtered);
+    };
+
     return (
-        <input
-            value={input}
-            onChange={(e) => setInput(e.target.value)}
-            onBlur={updateTaskList}
-            // onKeyDown={(e) => {
-            //     if (e.key === "Enter") updateTaskList();
-            // }}
-        />
+        <div>
+            <input
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+                onBlur={updateTaskList}
+                // onKeyDown={(e) => {
+                //     if (e.key === "Enter") updateTaskList();
+                // }}
+            />
+            <button onClick={removeTask}>
+                <ClearIcon />{" "}
+            </button>
+        </div>
     );
 };
 
