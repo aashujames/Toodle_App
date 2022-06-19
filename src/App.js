33add2120ab8f1f9eda5fde2d69923976ebf0615
Ashish Gupta <ashishgupt25@gmail.com>
2022-06-19 19:31:03 +0530

    sidebar created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
+import Sidebar from "./components/Sidebar/Sidebar";
 import TodayTask from "./components/TodayTask";
 
 function App() {
     return (
         <div className="main">
+            <Sidebar />
             <TodayTask />
         </div>
     );
---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../../styles/Sidebar.css";
+
+const Sidebar = () => {
+    return (
+        <div className="sidebar-container">
+            <h3>Today</h3>
+            <h3>Tomorrow</h3>
+            <h3>Weekly Report</h3>
+            <h3>Check Out</h3>
+        </div>
+    );
+};
+
+export default Sidebar;
