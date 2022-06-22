4a6d2e388e6cfe9b461e82cd0dae6bdd959cfa2d
Ashish Gupta <ashishgupt25@gmail.com>
2022-06-22 15:51:41 +0530

    Login page created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import Login from "./components/Login/Login";
 import Register from "./components/Login/Register";
 import Sidebar from "./components/Task/Sidebar/Sidebar";
 import TodayTask from "./components/Task/TodayTask";
@@ -7,7 +8,7 @@ import TodayTask from "./components/Task/TodayTask";
 function App() {
     return (
         <div className="main-container">
-            <Register />
+            <Login />
         </div>
         // <div className="main">
         //     <Sidebar />
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,7 +3,32 @@ import logo from "../../../src/logo.png";
 
 const Login = () => {
     return (
-        
+        <div className="base-container">
+            <div className="header">Login</div>
+            <img src={logo} alt="logo" className="image" />
+
+            <div className="form">
+                <div className="form-group">
+                    <label htmlFor="username">Username </label> <br />
+                    <input className="text-field" type="text" id="username" />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password">Password </label>
+                    <br />
+                    <input
+                        className="text-field"
+                        type="password"
+                        id="password"
+                    />
+                </div>
+            </div>
+
+            <div className="footer">
+                <button type="button" className="btn">
+                    Login
+                </button>
+            </div>
+        </div>
     );
 };
 
