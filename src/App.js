8b4d1ba855cf0949a3d31494a7a525f5c9136d3f
Ashish Gupta <ashishgupt25@gmail.com>
2022-06-23 11:15:23 +0530

    Home page created

--- src/App.js
+++ src/App.js
@@ -1,19 +1,52 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/Login/Login";
 import Register from "./components/Login/Register";
 import Sidebar from "./components/Task/Sidebar/Sidebar";
 import TodayTask from "./components/Task/TodayTask";
+import Home from "./components/Home/Home";
 
 function App() {
     return (
-        <div className="main-container">
-            <Login />
-        </div>
-        // <div className="main">
-        //     <Sidebar />
-        //     <TodayTask />
-        // </div>
+        <BrowserRouter>
+            <Routes>
+                <Route
+                    path="/"
+                    element={
+                        <div className="home-container">
+                            {" "}
+                            <Home />{" "}
+                        </div>
+                    }
+                />
+                {/* <Route
+                    path="/"
+                    element={
+                        <div className="main">
+                            <Sidebar />
+                            <TodayTask />
+                        </div>
+                    }
+                /> */}
+                <Route
+                    path="register"
+                    element={
+                        <div className="login-container">
+                            <Register />
+                        </div>
+                    }
+                ></Route>
+                <Route
+                    path="login"
+                    element={
+                        <div className="login-container">
+                            <Login />
+                        </div>
+                    }
+                ></Route>
+            </Routes>
+        </BrowserRouter>
     );
 }
 
---
+++ src/components/Home/Home.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../../styles/Home.css";
+import logo from "../../../src/logo.png";
+
+const Home = () => {
+    return (
+        <>
+            <nav className="navbar">
+                <img src={logo} alt="logo" className="image" />
+                <div className="nav-container">
+                    <Link to="register" className="link">
+                        Register
+                    </Link>
+                    <Link to="login" className="link">
+                        Login
+                    </Link>
+                </div>
+            </nav>
+            <div className="top">
+                <h1>Welcome to the Toodle App!</h1>
+                <div>For proper workflow Toodle is what you need.</div>
+                <div>Make your everyday an awesome day with Toodle.</div>
+                <h3>New User register to get access.</h3>
+            </div>
+        </>
+    );
+};
+
+export default Home;
